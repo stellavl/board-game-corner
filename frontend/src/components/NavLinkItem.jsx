8544a24be5aabd2245010cd98a6a4d149e86e09f
Fabio <stella.vlassi01@gmail.com>
2025-01-28 00:50:08 +0200

    disable text wrap

--- frontend/src/components/NavLinkItem.jsx
+++ frontend/src/components/NavLinkItem.jsx
@@ -4,7 +4,7 @@ import { Nav } from 'react-bootstrap';
 const NavLinkItem = ({ label, link, activeLink, setActiveLink }) => {
   return (
     <Nav.Link
-      className={`px-4 fs-5 ${activeLink === link ? 'text-decoration-underline' : ''}`}
+      className={`px-4 fs-5 text-nowrap ${activeLink === link ? 'text-decoration-underline' : ''}`}
       style={{ color: 'var(--color-soft-yellow)' }}
       onClick={() => setActiveLink(link)}
     >

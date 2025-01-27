5ae575f668deb0bd3bc1b408332b01874f5adf6f
Fabio <stella.vlassi01@gmail.com>
2025-01-28 00:19:24 +0200

    add interaction on hover

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Stack } from 'react-bootstrap';
 import { ChevronDown } from 'react-bootstrap-icons';
 
 const LoginButton = () => {
+  // State to manage hover effect
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <Button
-      className="login-button py-2 px-3 ms-4 fs-5 border border-1"
+      className="py-2 px-3 ms-4 fs-5 border border-1"
       style={{
-        color: 'var(--color-soft-yellow)', 
-        border: 'var(--color-soft-yellow)', 
-        backgroundColor: 'var(--color-blue)', 
+        color: 'var(--color-soft-yellow)',
+        border: isHovered ? 'var(--color-orange)' : 'var(--color-soft-yellow)',
+        backgroundColor: isHovered ? 'var(--color-orange)' : 'var(--color-blue)',
       }}
+      // Event handlers to manage hover state
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      Σύνδεση 
-      <ChevronDown className="ms-3"/>
+      <Stack direction="horizontal" gap={3}>
+        <span>Σύνδεση</span>
+        <ChevronDown />
+      </Stack>
     </Button>
   );
 };

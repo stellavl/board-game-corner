dd7239b6fac263cab026a0e7542ec3ceb35f53e7
Fabio <stella.vlassi01@gmail.com>
2025-01-27 23:51:52 +0200

    give .jsx extension to app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./pages/Home.jsx";
+import Header from "./components/Header.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;

e666a4184b7b5025125ff5f75b63b1b728cb8cd0
Fabio <stella.vlassi01@gmail.com>
2025-01-28 23:46:46 +0200

    add interaction on hover

--- frontend/src/components/OrangeButton.jsx
+++ frontend/src/components/OrangeButton.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const OrangeButton = ({ text }) => {
-  return (
-      <button 
-          className="btn"
-          style={{
-              color: 'var(--color-soft-yellow)',
-              backgroundColor: 'var(--color-orange)',
-          }}
-      >
-          {text}
-      </button>
-  );
+const OrangeButton = ({ text, size }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    return (
+        <button 
+            className={`btn ${size}`}
+            style={{
+                color: isHovered ? 'var(--color-orange)' : 'var(--color-soft-yellow)',
+                backgroundColor: isHovered ? 'var(--color-soft-yellow)' : 'var(--color-orange)',
+                border: isHovered ? '2px solid var(--color-orange)' : '2px solid var(--color-orange)',
+            }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            {text}
+        </button>
+    );
 };
 
 export default OrangeButton;

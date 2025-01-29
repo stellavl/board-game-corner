a95db85bcdc204b611cb98c015514ca5482bd08c
Fabio <stella.vlassi01@gmail.com>
2025-01-29 19:56:44 +0200

    make reservation form responsive

--- frontend/src/components/ReservationFormGeneral.jsx
+++ frontend/src/components/ReservationFormGeneral.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Form } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import OrangeButton from './OrangeButton'; 
 
 const ReservationForm = () => {
@@ -10,27 +10,37 @@ const ReservationForm = () => {
         Κάνε τώρα την κράτησή σου:
       </h6>
       <Container 
-        className="py-4 rounded align-items-center" 
+        className="py-4 rounded mx-auto" 
         style={{ 
           backgroundColor: 'var(--color-soft-orange)', 
           border: '2px solid var(--color-orange)',
-          maxWidth: '900px', // Optimized width so that all form fields are in one line
+          maxWidth: '900px', // Optimized width
         }}
       >
-        <div className="d-flex flex-wrap gap-3 justify-content-center w-100">
-          <Form.Select className="form-control w-auto">
-            <option>Παιχνιδοκαφέ</option>
-          </Form.Select>
-          <Form.Select className="form-control w-auto">
-            <option>Επιτραπέζιο</option>
-          </Form.Select>
-          <Form.Control type="number" placeholder="Πλήθος παικτών" className="form-control w-auto" />
-          <Form.Control type="date" className="form-control w-auto" />
-          <Form.Select className="form-control w-auto">
-            <option>Ώρα</option>
-          </Form.Select>
-        </div>
-        <div className="mt-4">
+        <Row className="g-3 row-cols-1 row-cols-lg-auto justify-content-center">
+          <Col>
+            <Form.Select className="form-control">
+              <option>Παιχνιδοκαφέ</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Select className="form-control">
+              <option>Επιτραπέζιο</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Control type="number" placeholder="Πλήθος παικτών" className="form-control" />
+          </Col>
+          <Col>
+            <Form.Control type="date" className="form-control" />
+          </Col>
+          <Col>
+            <Form.Select className="form-control">
+              <option>Ώρα</option>
+            </Form.Select>
+          </Col>
+        </Row>
+        <div className="mt-4 text-center">
           <OrangeButton text="ΣΥΝΕΧΕΙΑ"/>
         </div>
       </Container>

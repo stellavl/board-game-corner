77751a8474471ca2091f62679ae5a0f31e85b484
Fabio <stella.vlassi01@gmail.com>
2025-02-01 19:19:24 +0200

    use links for navigation

--- frontend/src/components/header/NavLinkItem.jsx
+++ frontend/src/components/header/NavLinkItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavLinkItem = ({ label, link, activeLink, setActiveLink }) => {
   return (
-    <Nav.Link
-      className={`px-4 fs-5 text-nowrap ${activeLink === link ? 'text-decoration-underline' : ''}`}
-      style={{ color: 'var(--color-soft-yellow)' }}
-      onClick={() => setActiveLink(link)}
-    >
-      {label}
-    </Nav.Link>
+    <Nav.Item>
+      <Link
+        to={`/${link}`}
+        className={`nav-link px-4 fs-5 text-nowrap ${activeLink === link ? 'text-decoration-underline' : ''}`}
+        style={{ color: 'var(--color-soft-yellow)' }}
+        onClick={() => setActiveLink(link)}
+      >
+        {label}
+      </Link>
+    </Nav.Item>
   );
 };
 

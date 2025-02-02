2a100798f05220b0cc5bee14334a2a1da620d464
Fabio <stella.vlassi01@gmail.com>
2025-02-02 23:00:48 +0200

    save board game lists in localStorage (temporary)

--- frontend/src/components/context/BoardGameContext.jsx
+++ frontend/src/components/context/BoardGameContext.jsx
@@ -1,30 +1,37 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 // Create context
 const BoardGameContext = createContext();
 
 // Create a provider component
 export const BoardGameProvider = ({ children }) => {
-    const [boardGameState, setBoardGameState] = useState({
+    // Initialize state from localStorage if it exists, otherwise use default values
+    const initialState = JSON.parse(localStorage.getItem('boardGameState')) || {
         hasPlayed: false,
         wantToPlay: false,
         boardGameCafesCount: 12,  // default
         isFavorite: false,  
-    });
+    };
+
+    const [boardGameState, setBoardGameState] = useState(initialState);
 
     // Handle checkbox and favorite toggling
     const handleCheckboxChange = (key) => {
-        setBoardGameState(prevState => ({
-            ...prevState,
-            [key]: !prevState[key]
-        }));
+        const newState = { 
+            ...boardGameState,
+            [key]: !boardGameState[key]
+        };
+        setBoardGameState(newState);
+        localStorage.setItem('boardGameState', JSON.stringify(newState)); // Save to localStorage
     };
 
     const toggleFavorite = () => {
-        setBoardGameState(prevState => ({
-            ...prevState,
-            isFavorite: !prevState.isFavorite
-        }));
+        const newState = { 
+            ...boardGameState,
+            isFavorite: !boardGameState.isFavorite
+        };
+        setBoardGameState(newState);
+        localStorage.setItem('boardGameState', JSON.stringify(newState)); // Save to localStorage
     };
 
     return (

f3022221c2aba290e1434977e62726a9d6947ca4
Fabio <stella.vlassi01@gmail.com>
2025-02-02 23:04:51 +0200

    extract board game data in separate file

---
+++ frontend/src/data/boardGamesData.js
@@ -0,0 +1,13 @@
+const boardGames = [
+    { name: 'Catan', type: 'Στρατηγικής', duration: '60’', players: '3-4 παίκτες', age: '10+', image: '/boardgamephotos/catan.png' },
+    { name: 'Risk', type: 'Στρατηγικής', duration: '120’', players: '2-6 παίκτες', age: '10+', image: '/images/risk.jpg' },
+    { name: 'Monopoly', type: 'Οικονομικό', duration: '90’', players: '2-6 παίκτες', age: '8+', image: '/images/monopoly.jpg' },
+    { name: 'Carcassonne', type: 'Τακτικής', duration: '45’', players: '2-5 παίκτες', age: '7+', image: '/images/carcassonne.jpg' },
+    { name: 'Ticket to Ride', type: 'Στρατηγικής', duration: '45-60’', players: '2-5 παίκτες', age: '8+', image: '/images/ticket_to_ride.jpg' },
+    { name: 'Pandemic', type: 'Στρατηγικής', duration: '45’', players: '2-4 παίκτες', age: '8+', image: '/images/pandemic.jpg' },
+    { name: 'Clue', type: 'Μυστήριο', duration: '45’', players: '3-6 παίκτες', age: '8+', image: '/images/clue.jpg' },
+    { name: 'Splendor', type: 'Οικονομικό', duration: '30’', players: '2-4 παίκτες', age: '10+', image: '/images/splendor.jpg' },
+    { name: '7 Wonders', type: 'Στρατηγικής', duration: '30’', players: '3-7 παίκτες', age: '10+', image: '/images/7wonders.jpg' },
+  ];
+  
+  export default boardGames;

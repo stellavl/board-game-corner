31783fde53d2f2f0465316c02f1702849077fe19
Fabio <stella.vlassi01@gmail.com>
2025-02-02 20:37:59 +0200

    add dynamic valid routes

--- frontend/src/components/common/Main.jsx
+++ frontend/src/components/common/Main.jsx
@@ -1,30 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate, matchPath } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import BoardGamesPage from "../../pages/BoardGamesPage";
 import BoardGameCafesPage from "../../pages/BoardGameCafesPage";
+import SpecificBoardGamePage from "../../pages/SpecificBoardGamePage";
 
-const validRoutes = ["/home", "/boardgames", "/boardgamecafes"];
+const validRoutePatterns = [
+  "/home",
+  "/boardgames",
+  "/boardgamecafes",
+  "/boardgames/:boardGameName", // Dynamic route
+];
+
+const isValidRoute = (pathname) => {
+  return validRoutePatterns.some((pattern) => matchPath(pattern, pathname));
+};
 
 const Main = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [lastValidRoute, setLastValidRoute] = useState("/home"); // Default to home
 
   useEffect(() => {
-    if (validRoutes.includes(location.pathname)) {
-      setLastValidRoute(location.pathname); // Update only when visiting a valid page
-    } else {
-      navigate(lastValidRoute, { replace: true }); // Redirect to last valid route
+    if (!isValidRoute(location.pathname)) {
+      navigate("/home", { replace: true }); // Redirect invalid routes
     }
-  }, [location.pathname, navigate, lastValidRoute]);
+  }, [location.pathname, navigate]);
 
   return (
-    <Routes>
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/boardgames" element={<BoardGamesPage />} />
-      <Route path="/boardgamecafes" element={<BoardGameCafesPage />} />
-    </Routes>
+    <div style={{ minHeight: "350px" }}>
+
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/boardgames" element={<BoardGamesPage />} />
+        <Route path="/boardgamecafes" element={<BoardGameCafesPage />} />
+        <Route path="/boardgames/:boardGameName" element={<SpecificBoardGamePage />} />
+      </Routes>
+      </div>
   );
 };
 

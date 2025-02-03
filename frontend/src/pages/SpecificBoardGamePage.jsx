91c047fe51fa6ece7fc7c70f0ff72789e5576996
Fabio <stella.vlassi01@gmail.com>
2025-02-03 23:16:07 +0200

    make reviews responsive to smaller screens

--- frontend/src/pages/SpecificBoardGamePage.jsx
+++ frontend/src/pages/SpecificBoardGamePage.jsx
@@ -1,7 +1,6 @@
-// pages/SpecificBoardGamePage.js
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";  
+import { Container, Row, Col, Offcanvas } from "react-bootstrap";  
 import BackButton from "../components/common/BackButton";
 import BoardGameOverview from "../components/boardgamepage/BoardGameOverview";  
 import { BoardGameProvider } from "../components/context/BoardGameContext";  
@@ -13,19 +12,62 @@ const SpecificBoardGamePage = () => {
     const location = useLocation();
     const boardGame = location.state?.boardGame;
 
+    const [showReviews, setShowReviews] = useState(false);
+
     if (!boardGame) {
         return <h1 className="text-center mt-5">Game Not Found</h1>;
     }
 
+    // Handle the toggle of reviews visibility
+    const handleShowReviews = () => setShowReviews(true);
+    const handleHideReviews = () => setShowReviews(false);
+
     return (
         <>
+            {/* Button to go back */}
             <div className="ms-3 mt-3">
                 <BackButton />
             </div>
 
             <BoardGameProvider>
-                <Container className="d-flex flex-column justify-content-center align-items-center">
-                    
+                {/* Layout for medium (md) and smaller screens */}
+                <Container className="d-flex d-md-none flex-column justify-content-center align-items-center">
+                    <Row>
+                        {/* BoardGameOverview comes first */}
+                        <Col xs={12} className="order-1 mt-5">
+                            <BoardGameOverview boardGame={boardGame} />
+                        </Col>
+
+                        {/* BoardGameImageAndDetails comes second */}
+                        <Col xs={12} className="order-2">
+                            <BoardGameImageAndDetails boardGame={boardGame} />
+                        </Col>
+
+                        {/* BoardGameDescription comes third */}
+                        <Col xs={12} className="order-3">
+                            <BoardGameDescription boardGame={boardGame} />
+                        </Col>
+
+                        {/* Reviews Button for small/medium screens */}
+                        <Col xs={12} className="order-4">
+                            <button onClick={handleShowReviews} className="btn w-100" style={{ backgroundColor: "var(--color-orange)", color: "var(--color-soft-yellow)" }}>
+                                Show Reviews
+                            </button>
+                        </Col>
+                    </Row>
+                </Container>
+
+                {/* Offcanvas for Reviews on small/medium screens */}
+                <Offcanvas show={showReviews} onHide={handleHideReviews} placement="end">
+                    <Offcanvas.Header closeButton>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Reviews />
+                    </Offcanvas.Body>
+                </Offcanvas>
+
+                {/* Layout for large screens (md and above) */}
+                <Container className="d-none d-md-flex flex-column justify-content-center align-items-center">
                     <Row>
                         <Col md={4} xs={6} className="mt-5">
                             <BoardGameImageAndDetails boardGame={boardGame} />
@@ -46,9 +88,7 @@ const SpecificBoardGamePage = () => {
                             <Reviews />
                         </Col>
                     </Row>
-                    
                 </Container>
-
             </BoardGameProvider>
         </>
     );

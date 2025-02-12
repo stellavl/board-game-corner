b90cda4f05ea58f93e3958d9861081917d7c8030
Fabio <stella.vlassi01@gmail.com>
2025-02-13 00:06:02 +0200

    add risk photo

--- frontend/src/data/boardGames.js
+++ frontend/src/data/boardGames.js
@@ -1,6 +1,6 @@
 const boardGames = [
   { id: 1, name: 'Catan', type: 'Στρατηγικής', duration: '60’', players: '3-4 παίκτες', age: '10+', image: '/boardgamephotos/catan.png' },
-  { id: 2, name: 'Risk', type: 'Στρατηγικής', duration: '120’', players: '2-6 παίκτες', age: '10+', image: '/images/risk.jpg' },
+  { id: 2, name: 'Risk', type: 'Στρατηγικής', duration: '120’', players: '2-6 παίκτες', age: '10+', image: '/boardgamephotos/risk.jpg'},
   { id: 3, name: 'Monopoly', type: 'Οικονομικό', duration: '90’', players: '2-6 παίκτες', age: '8+', image: '/images/monopoly.jpg' },
   { id: 4, name: 'Carcassonne', type: 'Τακτικής', duration: '45’', players: '2-5 παίκτες', age: '7+', image: '/images/carcassonne.jpg' },
   { id: 5, name: 'Ticket to Ride', type: 'Στρατηγικής', duration: '45-60’', players: '2-5 παίκτες', age: '8+', image: '/images/ticket_to_ride.jpg' },

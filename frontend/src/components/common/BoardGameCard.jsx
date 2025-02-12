0a37fb79b4417a3faa439f3c2ee963f2a98b269a
Fabio <stella.vlassi01@gmail.com>
2025-02-12 23:26:12 +0200

    use interaction on hover

--- frontend/src/components/common/BoardGameCard.jsx
+++ frontend/src/components/common/BoardGameCard.jsx
@@ -3,12 +3,26 @@ import { Card, Col } from "react-bootstrap";
 import BoardGameDetails from "./BoardGameDetails";
 
 const BoardGameCard = ({ boardGame, handleCardClick }) => {
+  const cardStyle = {
+    borderColor: "var(--color-orange)",
+    cursor: "pointer",
+    maxWidth: '180px',
+    perspective: '1000px',
+    transition: 'transform 0.3s ease-in-out'
+  };
+
+  const cardHoverStyle = {
+    transform: 'scale(1.05)'
+  };
+
   return (
     <Col xs={6} md={4} xl={3} className="mb-3">
       <Card
         className="border-2 rounded-3 mx-auto flip-card"
-        style={{ borderColor: "var(--color-orange)", cursor: "pointer", maxWidth: '180px', perspective: '1000px' }}
+        style={cardStyle}
         onClick={() => handleCardClick(boardGame)}
+        onMouseEnter={e => e.currentTarget.style.transform = cardHoverStyle.transform}
+        onMouseLeave={e => e.currentTarget.style.transform = 'none'}
       >
         <Card.Img
           variant="top"

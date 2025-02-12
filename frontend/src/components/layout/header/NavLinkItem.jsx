cdca32a54e93cd7edf870eeb97a97ed1abc35178
Fabio <stella.vlassi01@gmail.com>
2025-02-12 22:49:46 +0200

    replace active link equals with startsWith

--- frontend/src/components/layout/header/NavLinkItem.jsx
+++ frontend/src/components/layout/header/NavLinkItem.jsx
@@ -5,13 +5,15 @@ import { Link, useLocation } from 'react-router-dom';
 const NavLinkItem = ({ label, to, setActiveLink }) => {
   const location = useLocation(); // Get current route
 
+  const isActive = location.pathname.startsWith(to);
+
   return (
     <Nav.Item>
       <Link
-        to={to} // Use the correct full route path
-        className={`nav-link px-4 fs-5 text-nowrap ${location.pathname === to ? 'text-decoration-underline' : ''}`}
+        to={to} 
+        className={`nav-link px-4 fs-5 text-nowrap ${isActive ? 'text-decoration-underline' : ''}`}
         style={{ color: 'var(--color-soft-yellow)' }}
-        onClick={() => setActiveLink(to)} // Store full route path in state
+        onClick={() => setActiveLink(to)}
       >
         {label}
       </Link>

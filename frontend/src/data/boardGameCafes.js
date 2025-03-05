4a3a1f1b3bfff8b143731be340790d931e694a9d
Fabio <stella.vlassi01@gmail.com>
2025-03-06 00:59:01 +0200

    add more info about board game cafes

--- frontend/src/data/boardGameCafes.js
+++ frontend/src/data/boardGameCafes.js
@@ -1,14 +1,14 @@
 const boardGameCafes = [
-    { id: 1, name: 'Playhouse', city: 'Πάτρα' },
-    { id: 2, name: 'Playhouse', city: 'Αθήνα' },
-    { id: 3, name: 'PLAyCE', city: 'Αθήνα' },
-    { id: 4, name: 'Acapus', city: 'Αθήνα' },
-    { id: 5, name: 'Toy Stories', city: 'Αθήνα' },
-    { id: 6, name: 'Πανδοχείο του Δρακοφοίνικα', city: 'Αθήνα' },
-    { id: 7, name: 'Το Κελί', city: 'Αθήνα' },
-    { id: 8, name: 'Rubicon', city: 'Αθήνα' },
-    { id: 9, name: 'Superfly Cafe', city: 'Αθήνα' },
-    { id: 10, name: 'The Game Rules', city: 'Αθήνα' },
-  ];
-  
-  export default boardGameCafes;
+  { id: 1, name: 'Playhouse', city: 'Πάτρα', address: 'Ρήγα Φεραίου 127', phone: '2610243643' },
+  { id: 2, name: 'Playhouse', city: 'Αθήνα', address: 'Σκουζέ 3, Κορωπί', phone: '2103821200' },
+  { id: 3, name: 'PLAyCE', city: 'Αθήνα', address: 'Β. Λογοθετίδη 14, Αμπελόκηποι', phone: '2106990753' },
+  { id: 4, name: 'Acapus', city: 'Αθήνα', address: 'Β. Λάσκου 39 και Αγίου Φανουρίου, Παγκράτι', phone: '2107646573' },
+  { id: 5, name: 'Toy Stories', city: 'Αθήνα', address: 'Ηροδότου 32, Περιστέρι', phone: '2105780215' },
+  { id: 6, name: 'Πανδοχείο του Δρακοφοίνικα', city: 'Αθήνα', address: 'Ηλία Ηλιού 55, Νέος Κόσμος', phone: '2117351742' },
+  { id: 7, name: 'Το Κελί', city: 'Αθήνα', address: 'Κλαδά 20, Νέος Κόσμος', phone: '6942639746' },
+  { id: 8, name: 'Rubicon', city: 'Αθήνα', address: 'Ορφέως 10-12, Γαλάτσι', phone: '6972037766' },
+  { id: 9, name: 'Superfly Cafe', city: 'Αθήνα', address: 'Εμπεδοκλέους 28, Παγκράτι', phone: '2114046076' },
+  { id: 10, name: 'The Game Rules', city: 'Αθήνα', address: 'Αίαντος 10, Περιστέρι', phone: '2152151221' }
+];
+
+export default boardGameCafes;

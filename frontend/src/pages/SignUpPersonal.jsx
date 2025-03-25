40e22a45dad8fb94a082ba3a6a9a90252f62c071
Fabio <stella.vlassi01@gmail.com>
2025-03-25 23:06:57 +0200

    optimize sign up form

--- frontend/src/pages/SignUpPersonal.jsx
+++ frontend/src/pages/SignUpPersonal.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Container, Card, Form, Row, Col } from "react-bootstrap";
 import OrangeButton from "../components/common/OrangeButton";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { validatePersonalData } from "../components/utils/validations";
 
 const SignUpPersonal = () => {
@@ -32,160 +31,59 @@ const SignUpPersonal = () => {
         }
     };
 
+    const fields = [
+        { label: "Όνομα:", name: "firstName", type: "text" },
+        { label: "Επίθετο:", name: "lastName", type: "text" },
+        { label: "Email:", name: "email", type: "email" },
+        { label: "Τηλέφωνο Επικοινωνίας:", name: "phone", type: "text" },
+        { label: "Κωδικός Πρόσβασης:", name: "password", type: "password" },
+        { label: "Επανάληψη Κωδικού Πρόσβασης:", name: "confirmPassword", type: "password" },
+    ];
+
     return (
         <>
-         <Row>
-            <h3 className="text-center my-4" style={{ color: "var(--color-gray-purple)" }}>Δημιουργία Λογαριασμού</h3>
-        </Row>
-        <Container className="d-flex justify-content-center">
-            <Card className="p-4 border-2" style={{ borderColor: "var(--color-orange)", backgroundColor: "var(--color-soft-yellow)", width: "40rem" }}>
-                <Form onSubmit={handleSubmit}>
-                    <Row className="mb-3">
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Όνομα:</Form.Label>
-                        <div style={{ position: "relative" }}>
-                            <Form.Control 
-                                type="text" 
-                                name="firstName" 
-                                value={formData.firstName} 
-                                onChange={handleChange} 
-                                style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                isInvalid={!!errors.firstName}
-                            />
-                            {errors.firstName && (
-                                <FontAwesomeIcon
-                                    style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                />
-                            )}
-                            <Form.Control.Feedback type="invalid">
-                                {errors.firstName}
-                            </Form.Control.Feedback>
-                        </div>
-                            </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Επίθετο:</Form.Label>
-                                <div style={{ position: "relative" }}>
-                                    <Form.Control 
-                                        type="text" 
-                                        name="lastName" 
-                                        value={formData.lastName} 
-                                        onChange={handleChange} 
-                                        style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                        isInvalid={!!errors.lastName}
-                                    />
-                                    {errors.lastName && (
-                                        <FontAwesomeIcon
-                                            style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                        />
-                                    )}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.lastName}
-                                    </Form.Control.Feedback>
-                                </div>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
-                    <Row className="mb-3">
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Email:</Form.Label>
-                                <div style={{ position: "relative" }}>
-                                    <Form.Control 
-                                        type="email" 
-                                        name="email" 
-                                        value={formData.email} 
-                                        onChange={handleChange} 
-                                        style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                        isInvalid={!!errors.email}
-                                    />
-                                    {errors.email && (
-                                        <FontAwesomeIcon
-                                            style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                        />
-                                    )}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.email}
-                                    </Form.Control.Feedback>
-                                </div>
-                            </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Τηλέφωνο Επικοινωνίας:</Form.Label>
-                                <div style={{ position: "relative" }}>
-                                    <Form.Control 
-                                        type="text" 
-                                        name="phone" 
-                                        value={formData.phone} 
-                                        onChange={handleChange} 
-                                        style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                        isInvalid={!!errors.phone}
-                                    />
-                                    {errors.phone && (
-                                        <FontAwesomeIcon
-                                            style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                        />
-                                    )}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.phone}
-                                    </Form.Control.Feedback>
-                                </div>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
-                    <Row className="mb-3">
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Κωδικός Πρόσβασης:</Form.Label>
-                                <div style={{ position: "relative" }}>
-                                    <Form.Control 
-                                        type="password" 
-                                        name="password" 
-                                        value={formData.password} 
-                                        onChange={handleChange} 
-                                        style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                        isInvalid={!!errors.password}
-                                    />
-                                    {errors.password && (
-                                        <FontAwesomeIcon
-                                            style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                        />
-                                    )}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.password}
-                                    </Form.Control.Feedback>
-                                </div>
-                            </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                            <Form.Group>
-                                <Form.Label>Επανάληψη Κωδικού Πρόσβασης:</Form.Label>
-                                <div style={{ position: "relative" }}>
-                                    <Form.Control 
-                                        type="password" 
-                                        name="confirmPassword" 
-                                        value={formData.confirmPassword} 
-                                        onChange={handleChange} 
-                                        style={{ backgroundColor: "transparent", borderColor: 'var(--color-orange)' }}
-                                        isInvalid={!!errors.confirmPassword}
-                                    />
-                                    {errors.confirmPassword && (
-                                        <FontAwesomeIcon
-                                            style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)", color: "red" }}
-                                        />
-                                    )}
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.confirmPassword}
-                                    </Form.Control.Feedback>
-                                </div>
-                            </Form.Group>
-                        </Col>
-                    </Row>
+            <Row>
+                <h3 className="text-center my-4" style={{ color: "var(--color-gray-purple)" }}>
+                    Δημιουργία Λογαριασμού
+                </h3>
+            </Row>
+            <Container className="d-flex justify-content-center">
+                <Card className="p-4 border-2" style={{ borderColor: "var(--color-orange)", backgroundColor: "var(--color-soft-yellow)", width: "40rem" }}>
+                    <Form onSubmit={handleSubmit}>
+                        {fields.reduce((rows, { label, name, type }, index) => {
+                            if (index % 2 === 0) {
+                                rows.push([]);
+                            }
+                            rows[rows.length - 1].push({ label, name, type });
+                            return rows;
+                        }, []).map((rowFields, rowIndex) => (
+                            <Row className="mb-4" key={rowIndex}>
+                                {rowFields.map(({ label, name, type }) => (
+                                    <Col md={6} key={name}>
+                                        <Form.Group>
+                                            <Form.Label>{label}</Form.Label>
+                                            <div style={{ position: "relative" }}>
+                                                <Form.Control
+                                                    type={type}
+                                                    name={name}
+                                                    value={formData[name]}
+                                                    onChange={handleChange}
+                                                    style={{ backgroundColor: "transparent", borderColor: "var(--color-orange)" }}
+                                                    isInvalid={!!errors[name]}
+                                                />
+                                                <Form.Control.Feedback 
+                                                    type="invalid"
+                                                    className="position-absolute mb-1"
+                                                    style={{ bottom: "-1.5rem" }}
+                                                >
+                                                    {errors[name]}
+                                                </Form.Control.Feedback>
+                                            </div>
+                                        </Form.Group>
+                                    </Col>
+                                ))}
+                            </Row>
+                        ))}
 
                     <Row className="d-flex justify-content-center">
                         <Col md={6} className="d-flex justify-content-center">

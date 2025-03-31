5a05b0091443bf2f978abbd7eb268cff6391966d
Fabio <stella.vlassi01@gmail.com>
2025-03-31 19:36:52 +0300

    create reservations mock Data

---
+++ frontend/src/data/reservationsData.js
@@ -0,0 +1,92 @@
+const reservationsData = [
+    {
+        cafe: "Playhouse | Athens",
+        date: "28/1/25",
+        time: "20:00",
+        players: 3,
+        boardGame: "Catan",
+        status: "Αναμονή για επιβεβαίωση"
+    },
+    {
+        cafe: "PLAyCE | Athens",
+        date: "31/1/25",
+        time: "19:00",
+        players: 4,
+        boardGame: "Ticket to ride",
+        status: "Εγκρίθηκε"
+    },
+    {
+        cafe: "Playhouse | Athens",
+        date: "5/4/25",
+        time: "21:00",
+        players: 5,
+        boardGame: "Phase 10",
+        status: "Εγκρίθηκε"
+    },
+    {
+        cafe: "Game Hub | Thessaloniki",
+        date: "12/5/25",
+        time: "18:30",
+        players: 2,
+        boardGame: "Carcassonne",
+        status: "Αναμονή για επιβεβαίωση"
+    },
+    {
+        cafe: "Playhouse | Patras",
+        date: "30/4/25",
+        time: "21:15",
+        players: 6,
+        boardGame: "Bang",
+        status: "Απορρίφθηκε"
+    },
+    {
+        cafe: "BoardGames Plus | Patras",
+        date: "15/6/24",
+        time: "22:00",
+        players: 4,
+        boardGame: "Splendor",
+        status: "Απορρίφθηκε"
+    },
+    {
+        cafe: "Playhouse | Athens",
+        date: "7/7/23",
+        time: "19:45",
+        players: 3,
+        boardGame: "Pandemic",
+        status: "Εγκρίθηκε"
+    },
+    {
+        cafe: "PLAyCE | Athens",
+        date: "22/8/23",
+        time: "20:15",
+        players: 5,
+        boardGame: "Azul",
+        status: "Εγκρίθηκε"
+    },
+    {
+        cafe: "BoardGames World | Thessaloniki",
+        date: "1/9/24",
+        time: "17:30",
+        players: 2,
+        boardGame: "7 Wonders",
+        status: "Αναμονή για επιβεβαίωση"
+    },
+    {
+        cafe: "Game Hub | Athens",
+        date: "20/10/23",
+        time: "16:00",
+        players: 4,
+        boardGame: "Terraforming Mars",
+        status: "Απορρίφθηκε"
+    },
+    {
+        cafe: "Playhouse | Athens",
+        date: "10/11/23",
+        time: "18:30",
+        players: 6,
+        boardGame: "Twilight Struggle",
+        status: "Εγκρίθηκε"
+    }
+];
+
+export default reservationsData;

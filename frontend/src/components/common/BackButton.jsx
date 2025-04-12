f6983441aa039b99a786048fd1425ffbeec7b521
Fabio <stella.vlassi01@gmail.com>
2025-04-12 18:30:46 +0300

    update back button

--- frontend/src/components/common/BackButton.jsx
+++ frontend/src/components/common/BackButton.jsx
@@ -3,22 +3,21 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import OrangeButton from "./OrangeButton";  
 import { useNavigate } from "react-router-dom";
 
-const BackButton = ({text = "Επιστροφή"}) => {
+const BackButton = ({ text = "Επιστροφή" }) => {
     const navigate = useNavigate();
 
-    // Handle going back to the previous page
     const handleGoBack = () => {
-        navigate(-1); 
+        navigate(-1);
     };
 
     return (
         <OrangeButton
             text={
-                <>
-                <FontAwesomeIcon icon={faArrowLeft} className="me-2" /> {text}
-                </>
+                <span className="d-inline-flex align-items-center text-nowrap">
+                    <FontAwesomeIcon icon={faArrowLeft} className="me-2" /> {text}
+                </span>
             }
-            className="text-decoration-none position-relative top-3 start-3" 
+            className="text-decoration-none position-relative top-3 start-3"
             onClick={handleGoBack}
             size="btn-sm"
         />

932b8bb95c78f500eb5c1021472687626d4fb869
Fabio <stella.vlassi01@gmail.com>
2025-04-12 18:30:52 +0300

    update reservations data

--- frontend/src/data/reservationsData.js
+++ frontend/src/data/reservationsData.js
@@ -5,7 +5,9 @@ const reservationsData = [
         time: "20:00",
         players: 3,
         boardGame: "Catan",
-        status: "Αναμονή για επιβεβαίωση"
+        status: "Αναμονή για επιβεβαίωση",
+        customerName: "Γιάννης Παπαδόπουλος",
+        phoneNumber: "690 123 4567"
     },
     {
         cafe: "PLAyCE | Athens",
@@ -13,7 +15,9 @@ const reservationsData = [
         time: "19:00",
         players: 4,
         boardGame: "Ticket to ride",
-        status: "Εγκρίθηκε"
+        status: "Εγκρίθηκε",
+        customerName: "Μαρία Παπακωνσταντίνου",
+        phoneNumber: "690 234 5678"
     },
     {
         cafe: "Playhouse | Athens",
@@ -21,7 +25,9 @@ const reservationsData = [
         time: "21:00",
         players: 5,
         boardGame: "Phase 10",
-        status: "Εγκρίθηκε"
+        status: "Εγκρίθηκε",
+        customerName: "Αλέξανδρος Ιωαννίδης",
+        phoneNumber: "690 345 6789"
     },
     {
         cafe: "Game Hub | Thessaloniki",
@@ -29,7 +35,9 @@ const reservationsData = [
         time: "18:30",
         players: 2,
         boardGame: "Carcassonne",
-        status: "Αναμονή για επιβεβαίωση"
+        status: "Αναμονή για επιβεβαίωση",
+        customerName: "Ελένη Δασκαλάκη",
+        phoneNumber: "690 456 7890"
     },
     {
         cafe: "Playhouse | Patras",
@@ -37,7 +45,9 @@ const reservationsData = [
         time: "21:15",
         players: 6,
         boardGame: "Bang",
-        status: "Απορρίφθηκε"
+        status: "Απορρίφθηκε",
+        customerName: "Μιχάλης Βασιλείου",
+        phoneNumber: "690 567 8901"
     },
     {
         cafe: "BoardGames Plus | Patras",
@@ -45,7 +55,9 @@ const reservationsData = [
         time: "22:00",
         players: 4,
         boardGame: "Splendor",
-        status: "Απορρίφθηκε"
+        status: "Απορρίφθηκε",
+        customerName: "Σοφία Παπαδάκη",
+        phoneNumber: "690 678 9012"
     },
     {
         cafe: "Playhouse | Athens",
@@ -53,7 +65,9 @@ const reservationsData = [
         time: "19:45",
         players: 3,
         boardGame: "Pandemic",
-        status: "Εγκρίθηκε"
+        status: "Εγκρίθηκε",
+        customerName: "Δημήτρης Μαρκόπουλος",
+        phoneNumber: "690 789 0123"
     },
     {
         cafe: "PLAyCE | Athens",
@@ -61,7 +75,9 @@ const reservationsData = [
         time: "20:15",
         players: 5,
         boardGame: "Azul",
-        status: "Εγκρίθηκε"
+        status: "Εγκρίθηκε",
+        customerName: "Ολυμπία Τζιώρα",
+        phoneNumber: "690 890 1234"
     },
     {
         cafe: "BoardGames World | Thessaloniki",
@@ -69,7 +85,9 @@ const reservationsData = [
         time: "17:30",
         players: 2,
         boardGame: "7 Wonders",
-        status: "Αναμονή για επιβεβαίωση"
+        status: "Αναμονή για επιβεβαίωση",
+        customerName: "Δανιήλ Χατζηγεωργίου",
+        phoneNumber: "690 901 2345"
     },
     {
         cafe: "Game Hub | Athens",
@@ -77,7 +95,9 @@ const reservationsData = [
         time: "16:00",
         players: 4,
         boardGame: "Terraforming Mars",
-        status: "Απορρίφθηκε"
+        status: "Απορρίφθηκε",
+        customerName: "Σοφία Κωνσταντίνου",
+        phoneNumber: "690 012 3456"
     },
     {
         cafe: "Playhouse | Athens",
@@ -85,7 +105,9 @@ const reservationsData = [
         time: "18:30",
         players: 6,
         boardGame: "Twilight Struggle",
-        status: "Εγκρίθηκε"
+        status: "Εγκρίθηκε",
+        customerName: "Ηλίας Λεβέντης",
+        phoneNumber: "690 123 4567"
     }
 ];
 

3c1e91f3aec6551d2c4727bd1200bffe5bbdf6dd
Fabio <stella.vlassi01@gmail.com>
2025-04-17 15:44:00 +0300

    update db config

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -1,18 +1,17 @@
 INSERT INTO registered_user (first_name, last_name, email, password, phone_number) VALUES
-('John', 'Doe', 'john.doe@example.com', 'password123', 1234567890),
-('Jane', 'Smith', 'jane.smith@example.com', 'password456', 9876543210);
+('Stella', 'Vlassi', 'stella.vlassi@example.com', 'password123', '6912345678')
 
 INSERT INTO board_game_cafe (name, city, address, phone_number, email, password, photo) VALUES
-('Cafe A', 'City A', '123 Main St', 1234567890, 'cafeA@example.com', 'cafePassword1', 'photoA.jpg'),
-('Cafe B', 'City B', '456 Elm St', 9876543210, 'cafeB@example.com', 'cafePassword2', 'photoB.jpg');
+('Cafe A', 'City A', '123 Main St', '1234567890', 'cafeA@example.com', 'cafePassword1', 'photoA.jpg'),
+('Cafe B', 'City B', '456 Elm St', '9876543210', 'cafeB@example.com', 'cafePassword2', 'photoB.jpg');
 
 INSERT INTO board_game (name) VALUES
 ('Board Game 1'),
 ('Board Game 2');
 
 INSERT INTO reservation (date, time, players_no, customer_first_name, customer_last_name, customer_email, customer_phone, status, board_game_id, board_game_cafe_id, registered_user_id) VALUES
-('2023-10-01', '18:00:00', 4, 'John', 'Doe', 'john.doe@example.com', 1234567890, 'confirmed', 1, 1, 1),
-('2023-10-02', '19:00:00', 2, 'Jane', 'Smith', 'jane.smith@example.com', 9876543210, 'pending', 2, 2, 2);
+('2023-10-01', '18:00:00', 4, 'John', 'Doe', 'john.doe@example.com', '1234567890', 'confirmed', 1, 1, NULL),
+('2023-10-02', '19:00:00', 2, 'Jane', 'Smith', 'jane.smith@example.com', '9876543210', 'pending', 2, 2, null);
 
 INSERT INTO user_board_game_list (is_favorite, is_have_played, is_want_to_play, reg_user_id, board_game_id) VALUES
 (true, true, false, 1, 1),
--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,22 +1,23 @@
+DROP DATABASE IF EXISTS board_game_corner;
 CREATE DATABASE IF NOT EXISTS board_game_corner;
 USE board_game_corner;
 
 CREATE TABLE IF NOT EXISTS `registered_user` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
 	`first_name` varchar(255) NOT NULL,
 	`last_name` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL UNIQUE,
 	`password` varchar(255) NOT NULL,
-	`phone_number` int NOT NULL,
+	`phone_number` varchar(10) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `board_game_cafe` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
 	`name` varchar(255) NOT NULL,
 	`city` varchar(255) NOT NULL,
 	`address` varchar(255) NOT NULL,
-	`phone_number` int NOT NULL,
+	`phone_number` varchar(10) NOT NULL,
 	`email` varchar(255) NOT NULL UNIQUE,
 	`password` varchar(255) NOT NULL UNIQUE,
 	`photo` varchar(255) NOT NULL,
@@ -24,54 +25,53 @@ CREATE TABLE IF NOT EXISTS `board_game_cafe` (
 );
 
 CREATE TABLE IF NOT EXISTS `reservation` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
 	`date` date NOT NULL,
 	`time` time NOT NULL,
-	`players_no` int NOT NULL,
+	`players_no` bigint NOT NULL,
 	`customer_first_name` varchar(255) NOT NULL,
 	`customer_last_name` varchar(255) NOT NULL,
 	`customer_email` varchar(255) NOT NULL,
-	`customer_phone` int NOT NULL,
+	`customer_phone` bigint NOT NULL,
 	`status` varchar(255) NOT NULL,
-	`board_game_id` int NOT NULL,
-	`board_game_cafe_id` int NOT NULL,
-	`registered_user_id` int,
+	`board_game_id` bigint NOT NULL,
+	`board_game_cafe_id` bigint NOT NULL,
+	`registered_user_id` bigint,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `user_board_game_list` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
 	`is_favorite` boolean NOT NULL,
 	`is_have_played` boolean NOT NULL,
 	`is_want_to_play` boolean NOT NULL,
-	`reg_user_id` int NOT NULL,
-	`board_game_id` int NOT NULL,
+	`reg_user_id` bigint NOT NULL,
+	`board_game_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `board_game` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
 	`name` varchar(255) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `review` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
-	`stars` int NOT NULL,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
+	`stars` bigint NOT NULL,
 	`description` text NOT NULL,
 	`created_on` datetime NOT NULL,
-	`board_game_id` int NOT NULL,
-	`reg_user_id` int NOT NULL,
+	`board_game_id` bigint NOT NULL,
+	`reg_user_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `board_game_catalog` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
-	`board_game_id` int NOT NULL,
-	`board_game_cafe_id` int NOT NULL,
+	`id` bigint AUTO_INCREMENT NOT NULL UNIQUE,
+	`board_game_id` bigint NOT NULL,
+	`board_game_cafe_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
 
 
 ALTER TABLE `reservation` ADD CONSTRAINT `reservation_fk9` FOREIGN KEY (`board_game_id`) REFERENCES `board_game`(`id`);
@@ -89,3 +89,58 @@ ALTER TABLE `review` ADD CONSTRAINT `review_fk5` FOREIGN KEY (`reg_user_id`) REF
 ALTER TABLE `board_game_catalog` ADD CONSTRAINT `board_game_catalog_fk1` FOREIGN KEY (`board_game_id`) REFERENCES `board_game`(`id`);
 
 ALTER TABLE `board_game_catalog` ADD CONSTRAINT `board_game_catalog_fk2` FOREIGN KEY (`board_game_cafe_id`) REFERENCES `board_game_cafe`(`id`);
+
+DELIMITER $$
+
+-- Add triggers to generates a random 10-digit number as an ID in all tables
+
+CREATE TRIGGER `before_insert_registered_user`
+BEFORE INSERT ON `registered_user`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); 
+END$$
+
+CREATE TRIGGER `before_insert_board_game_cafe`
+BEFORE INSERT ON `board_game_cafe`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+CREATE TRIGGER `before_insert_reservation`
+BEFORE INSERT ON `reservation`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+CREATE TRIGGER `before_insert_user_board_game_list`
+BEFORE INSERT ON `user_board_game_list`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+CREATE TRIGGER `before_insert_board_game`
+BEFORE INSERT ON `board_game`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+CREATE TRIGGER `before_insert_review`
+BEFORE INSERT ON `review`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+CREATE TRIGGER `before_insert_board_game_catalog`
+BEFORE INSERT ON `board_game_catalog`
+FOR EACH ROW
+BEGIN
+    SET NEW.id = FLOOR(1000000000 + (RAND() * 8999999999)); -- Generates a random 10-digit number
+END$$
+
+DELIMITER ;

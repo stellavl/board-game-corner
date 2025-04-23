fadc86855cae878e0fa982c06e6615ad639b12dd
Fabio <stella.vlassi01@gmail.com>
2025-04-23 11:48:57 +0300

    fix typo

--- backend/src/routes/user-route.js
+++ backend/src/routes/user-route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserByIdController } from "../controllers/user-contoller.js";
+import { getUserByIdController } from "../controllers/user-controller.js";
 
 const router = express.Router();
 

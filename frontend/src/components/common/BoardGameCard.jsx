1b6aec4a5d79c3a7a6f3c8f09e1d148fd3ad5e04
Fabio <stella.vlassi01@gmail.com>
2025-05-10 18:28:39 +0300

    Remove unneseccary import

--- frontend/src/components/common/BoardGameCard.jsx
+++ frontend/src/components/common/BoardGameCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card, Col } from "react-bootstrap";
 import BoardGameDetails from "./BoardGameDetails";
 
--- frontend/src/components/common/BoardGameCards.jsx
+++ frontend/src/components/common/BoardGameCards.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Row, Pagination } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import usePagination from "../../hooks/usePagination";

3533e5147eb142b17cf1e5d85e481e7f8e751e38
Fabio <stella.vlassi01@gmail.com>
2025-05-17 18:02:23 +0300

    Optimise pagination handling

--- frontend/src/components/common/BoardGameCards.jsx
+++ frontend/src/components/common/BoardGameCards.jsx
@@ -30,35 +30,39 @@ const BoardGameCards = ({
       </Row>
 
       {/* Pagination Controls */}
-      <Pagination className="justify-content-center mt-3">
-        <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
+        {boardGames.length > 0 && (
+          <Pagination className="justify-content-center mt-3">
+            <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
 
-        {currentPage > 2 && (
-          <>
-            <Pagination.Item onClick={() => handlePageChange(1)}>1</Pagination.Item>
-            {currentPage > 3 && <Pagination.Ellipsis />}
-          </>
-        )}
+            {currentPage > 2 && (
+              <>
+                <Pagination.Item onClick={() => handlePageChange(1)}>1</Pagination.Item>
+                {currentPage > 3 && <Pagination.Ellipsis />}
+              </>
+            )}
 
-        {currentPage > 1 && (
-          <Pagination.Item onClick={() => handlePageChange(currentPage - 1)}>{currentPage - 1}</Pagination.Item>
-        )}
+            {currentPage > 1 && (
+              <Pagination.Item onClick={() => handlePageChange(currentPage - 1)}>{currentPage - 1}</Pagination.Item>
+            )}
 
-        <Pagination.Item active>{currentPage}</Pagination.Item>
+            <Pagination.Item active>{currentPage}</Pagination.Item>
 
-        {currentPage < totalPages && (
-          <Pagination.Item onClick={() => handlePageChange(currentPage + 1)}>{currentPage + 1}</Pagination.Item>
-        )}
+            {currentPage < totalPages && (
+              <Pagination.Item onClick={() => handlePageChange(currentPage + 1)}>{currentPage + 1}</Pagination.Item>
+            )}
 
-        {currentPage < totalPages - 1 && (
-          <>
-            {currentPage < totalPages - 2 && <Pagination.Ellipsis />}
-            <Pagination.Item onClick={() => handlePageChange(totalPages)}>{totalPages}</Pagination.Item>
-          </>
-        )}
-
-        <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+            {currentPage < totalPages - 1 && (
+              <>
+                {currentPage < totalPages - 2 && <Pagination.Ellipsis />}
+                <Pagination.Item onClick={() => handlePageChange(totalPages)}>{totalPages}</Pagination.Item>
+              </>
+            )}
+            <Pagination.Next
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage >= totalPages}
+            />
       </Pagination>
+      )}
     </div>
   );
 };

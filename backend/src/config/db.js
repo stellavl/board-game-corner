86575d55305270e02173214e441491e932c178ca
Fabio <stella.vlassi01@gmail.com>
2025-05-29 20:04:52 +0300

    update db config to allow heroku JAWSDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,30 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import url from 'url';
 
-dotenv.config({ path: '../.env' }); 
-
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
+dotenv.config({ path: '../.env' });
 
 export const connectToDatabase = async () => {
-  return await mysql.createConnection(dbConfig);
-};
+  if (process.env.JAWSDB_URL) {
+    // Parse the JAWSDB_URL for better control
+    const { hostname, port, pathname, auth } = new url.URL(process.env.JAWSDB_URL);
+    const [user, password] = auth.split(':');
+    const database = pathname.split('/')[1];
+
+    return await mysql.createConnection({
+      host: hostname,
+      port,
+      user,
+      password,
+      database,
+    });
+  } else {
+    // Local development
+    return await mysql.createConnection({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'user',
+      password: process.env.DB_PASSWORD || 'user_password',
+      database: process.env.DB_NAME || 'board_game_corner',
+    });
+  }
+};  

b0fad43edb5f2794db78250b005febc5f482fbf3
Fabio <stella.vlassi01@gmail.com>
2025-05-29 20:21:51 +0300

    Fix bug

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-import loginPersonal from './login-route.js';
-import loginAdmin from './login-route.js';
+import login from './login-route.js';
 import userRoute from "./user-route.js";
 import adminRoute from "./admin-route.js";
 import boardGameRoute from './board-game-route.js';
@@ -9,8 +8,7 @@ import reservationRoute from './reservation-route.js';
 
 const router = Router();
 
-router.use('/', loginPersonal);
-router.use('/', loginAdmin);
+router.use('/', login);
 router.use('/', userRoute);
 router.use('/', adminRoute);
 router.use('/', boardGameRoute);

40f8bb8a04ffcf65d13ba9bdfcda45d52aa5949c
Fabio <stella.vlassi01@gmail.com>
2025-05-30 18:55:13 +0300

    Comment out all routes except login to debug

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -10,15 +10,15 @@ const router = Router();
 
 console.log("Mounting login routes");
 router.use('/', login);
-console.log("Mounting user routes");
-router.use('/', userRoute);
-console.log("Mounting admin routes");
-router.use('/', adminRoute);
-console.log("Mounting board game routes");
-router.use('/', boardGameRoute);
-console.log("Mounting cafe routes");
-router.use("/", cafeRoutes);
-console.log("Mounting reservation routes");
-router.use("/", reservationRoute);
+// console.log("Mounting user routes");
+// router.use('/', userRoute);
+// console.log("Mounting admin routes");
+// router.use('/', adminRoute);
+// console.log("Mounting board game routes");
+// router.use('/', boardGameRoute);
+// console.log("Mounting cafe routes");
+// router.use("/", cafeRoutes);
+// console.log("Mounting reservation routes");
+// router.use("/", reservationRoute);
 
 export default router;

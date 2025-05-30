243ad398ed69724f2f4dd9eb111d18d7344839d5
Fabio <stella.vlassi01@gmail.com>
2025-05-30 18:07:52 +0300

    Add logs for debugging

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -8,11 +8,17 @@ import reservationRoute from './reservation-route.js';
 
 const router = Router();
 
+console.log("Mounting login routes");
 router.use('/', login);
+console.log("Mounting user routes");
 router.use('/', userRoute);
+console.log("Mounting admin routes");
 router.use('/', adminRoute);
+console.log("Mounting board game routes");
 router.use('/', boardGameRoute);
+console.log("Mounting cafe routes");
 router.use("/", cafeRoutes);
+console.log("Mounting reservation routes");
 router.use("/", reservationRoute);
 
 export default router;

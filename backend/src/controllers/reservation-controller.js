1d9b5087a2b906e710ffca413e6ef9efe4e1ae4d
Fabio <stella.vlassi01@gmail.com>
2025-06-14 18:26:57 +0300

    Remove unesseccary email

--- backend/src/controllers/reservation-controller.js
+++ backend/src/controllers/reservation-controller.js
@@ -8,7 +8,8 @@ import {
 export const createReservationController = async (req, res) => {
   try {
     const reservationData = req.body;
-    const { reservationId, email } = await createReservationService(reservationData);
+    console
+    const { reservationId } = await createReservationService(reservationData);
     res.status(201).json({ reservationId });
   } catch (err) {
     res.status(400).json({ error: err.message });
